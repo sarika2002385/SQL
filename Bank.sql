c256e0ec3349d0c32ea41ff1c6d8778b9523f41a
sarika <Sarikaxworkz@gmail.com>
2024-06-26 09:54:44 +0530

    SQL-task

---
+++ Bank.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE Bank;
+CREATE Table bank(id int ,bank_name varchar(20), bank_balance bigint,customer_name char );
+
+CREATE DATABASE laptop;
+CREATE Table laptop(id int,laptop_brand varchar(20), laptop_cost bigint);
+
+CREATE DATABASE Employee;
+CREATE Table Employee(id int,Emp_name char(10),Emp_salary bigint,Emp_dob int);
+
+ 
+
